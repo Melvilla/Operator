854321595c0786aabd6dcd8ef325f8bb92128236
JustinGonsalves <justin.gonsalves@btinternet.com>
2025-09-17 12:57:39 +0100

    Finished UI_stage functionality and began login page mockup

--- Assets/DynamicScreens/Scripts/DynamicUIDisplayScreen.cs
+++ Assets/DynamicScreens/Scripts/DynamicUIDisplayScreen.cs
@@ -1,16 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class DynamicUIDisplayScreen : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] LayerMask RaycastMask = ~0;
+    [SerializeField] float RaycastDistance = 5.0f;
+    [SerializeField] UnityEvent<Vector2> OnCursorInput = new();
 
     // Update is called once per frame
     void Update()
     {
-        
+#if ENABLE_LEGACY_INPUT_MANAGER
+        Vector3 MousePosition = Input.mousePosition;
+#else
+        Vector3 MousePosition = UnityEngine.InputSystem.Mouse.current.position.ReadValue();
+#endif // ENABLE_LEGACY_INPUT_MANAGER
+
+        // Construct our ray from the mouse position
+        Ray MouseRay = Camera.main.ScreenPointToRay(MousePosition);
+
+        // Perform the raycase
+        RaycastHit HitResult;
+        if (Physics.Raycast(MouseRay, out HitResult, RaycastDistance, RaycastMask, QueryTriggerInteraction.Ignore))
+        {
+            // Ignore if not us
+            if (HitResult.collider.gameObject != gameObject)
+                return;
+            
+            OnCursorInput.Invoke(HitResult.textureCoord);  
+        }
     }
 }
